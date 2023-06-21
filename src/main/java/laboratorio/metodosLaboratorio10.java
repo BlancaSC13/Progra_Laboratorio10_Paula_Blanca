ee3c77a6f4841e63aaa1f5acfaeb726fed6346a2
BlancaSC13 <blanca.segura@ucr.ac.cr>
2023-06-21 09:49:44 -0600

    método diagonalDifference

--- src/main/java/laboratorio/metodosLaboratorio10.java
+++ src/main/java/laboratorio/metodosLaboratorio10.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class metodosLaboratorio10 {
 
     public static int diagonalDifference(List<List<Integer>> arr) {
-        int [][] array = new int[3][3];
+        int [][] array = new int[arr.size()][arr.size()];
         int sumaDiagonal1=0;
         int sumaDiagonal2=0;
         for (int i = 0; i<array.length; i++){
@@ -23,8 +23,6 @@ public class metodosLaboratorio10 {
             sumaDiagonal2+=array[i][counter];
             counter++;
         }
-        int resta = sumaDiagonal1-sumaDiagonal2;
-        return Math.abs(resta);
+        return Math.abs(sumaDiagonal1-sumaDiagonal2);
     }
-
 }
