30f6d3f060f1a05b973dde369b095c0e00d460c5
BlancaSC13 <blanca.segura@ucr.ac.cr>
2023-06-28 12:04:43 -0600

    game with cells

--- src/main/java/laboratorio/metodoslaboratorio11.java
+++ src/main/java/laboratorio/metodoslaboratorio11.java
@@ -11,7 +11,23 @@ public class metodoslaboratorio11 {
 
         return result;
     }
-
+    public static int gameWithCells(int n, int m){
+        int packages=0;
+        if (m%4 ==0 && n ==2) {
+            packages = m / 4;
+        } else if (m==n) {
+            packages=m/n;
+        } else if (n<m) {
+            packages=n;
+        }else if(m<n){
+            packages=m;
+        }else if (n%m==0&&m==2){
+            packages=n/4;
+        } else if (m==n && m%n==0) {
+            packages= (m*n)/4;
+        }
+        return packages;
+    }
 
 
 }
