a9498840136390e5ef3673b0459f9994593e20af
Lani6 <chavespaula912@gmail.com>
2023-06-28 11:48:38 -0600

    Lab11

---
+++ src/main/java/laboratorio/metodoslaboratorio11.java
@@ -0,0 +1,17 @@
+package laboratorio;
+
+//Blanca, Daniela, Paula
+public class metodoslaboratorio11 {
+
+
+    public static long solve(int n, int m) {
+        // Write your code here
+
+        long result = (long)n*m-1;
+
+        return result;
+    }
+
+
+
+}
